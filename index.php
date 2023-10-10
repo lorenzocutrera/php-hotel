52943f930ea799329d04d4fe1509c7949f329783
Lorenzo Cutrera <lorenzoq3ra@gmail.com>
2023-10-10 17:48:09 +0200

    add table

--- index.php
+++ index.php
@@ -49,6 +49,8 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
 </head>
 
 <body>
@@ -63,7 +65,48 @@ $hotels = [
         </ul>
     <?php endforeach; ?>
 
+<!-- TABLE -->
 
+<table class="table p-5">
+  <thead>
+    <tr>
+    <?php foreach ($hotels as $hotel) : ?>
+      <th scope="col"><?php echo $hotel['name'] ?></th>
+      <?php endforeach; ?>   
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+    <?php foreach ($hotels as $hotel) : ?>
+      <td scope="col"><?php echo $hotel['description'] ?></td>
+      <?php endforeach; ?>
+    </tr>
+    <tr>
+    <?php foreach ($hotels as $hotel) : ?>
+      <td scope="col"><?php echo $hotel['parking'] ?></td>
+      <?php endforeach; ?>
+    </tr>
+    <tr>
+    <?php foreach ($hotels as $hotel) : ?>
+      <td scope="col"><?php echo $hotel['vote'] ?></td>
+      <?php endforeach; ?>
+    </tr>
+    <tr>
+    <?php foreach ($hotels as $hotel) : ?>
+      <td scope="col"><?php echo $hotel['distance_to_center'] ?></td>
+      <?php endforeach; ?>
+    </tr>
+  </tbody>
+</table>
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
 
 </html>
